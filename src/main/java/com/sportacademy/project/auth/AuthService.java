108bc5e093ea7c7dc53f6c740f51796574ad1534
Krystian Kowalski <Krystian.Kowalski@trw.com>
2020-03-17 16:33:29 +0100

    little auth

---
+++ src/main/java/com/sportacademy/project/auth/AuthService.java
@@ -0,0 +1,15 @@
+package com.sportacademy.project.auth;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+
+@Service
+public class AuthService {
+
+  public Boolean isAuthorization(String header) {
+    String login = System.getenv("login");
+    String pwd = System.getenv("pwd");
+    String loginAndpwd = new String(Base64Utils.decodeFromString(header));
+    return loginAndpwd.equals(login + "&" + pwd);
+  }
+}
--- src/main/java/com/sportacademy/project/employees/EmployeesController.java
+++ src/main/java/com/sportacademy/project/employees/EmployeesController.java
@@ -1,8 +1,10 @@
 package com.sportacademy.project.employees;
 
+import com.sportacademy.project.auth.AuthService;
 import java.util.Base64;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -23,43 +27,65 @@ import org.springframework.web.multipart.MultipartFile;
 public class EmployeesController {
 
   private EmployeeRepository employeeRepository;
-  private StorageService storageService;
+  private AuthService authService;
   private EmployeeService employeeService;
 
   @GetMapping
-  public Iterable<Employee> getAll() {
-    return employeeRepository.findAll();
+  public Iterable<Employee> getAll(@RequestHeader("Authorization") String auth) {
+    if (authService.isAuthorization(auth)) {
+      return employeeRepository.findAll();
+    } else {
+      throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+    }
   }
 
   @GetMapping("/{id}")
-  public Employee getOne(@PathVariable String id) {
-    return employeeRepository.findById(id).orElseGet(null);
+  public Employee getOne(@PathVariable String id, @RequestHeader("Authorization") String auth) {
+    if (authService.isAuthorization(auth)) {
+      return employeeRepository.findById(id).orElseGet(null);
+    } else {
+      throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+    }
   }
 
   @PostMapping("/{id}/file")
-  public void handleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String id) {
+  public void handleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String id, @RequestHeader("Authorization") String auth) {
     employeeService.storeFileForEmployee(file, id);
   }
 
   @GetMapping(value = "/{id}/file")
-  public String getFile(@PathVariable String id, Model model) {
-    Employee employee = employeeService.getEmployee(id);
-    return Base64.getEncoder()
-        .encodeToString(employee.getPermissions().getLifeguard().getImage().getData());
+  public String getFile(@PathVariable String id, Model model, @RequestHeader("Authorization") String auth) {
+    if (authService.isAuthorization(auth)) {
+      Employee employee = employeeService.getEmployee(id);
+      return Base64.getEncoder()
+          .encodeToString(employee.getPermissions().getLifeguard().getImage().getData());
+    } else {
+      throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+    }
   }
 
   @PostMapping
-  public Employee create(@RequestBody Employee employee) {
-    Employee empl = this.employeeRepository.insert(employee);
-    return empl;
+  public Employee create(@RequestBody Employee employee, @RequestHeader("Authorization") String auth) {
+    if (authService.isAuthorization(auth)) {
+      Employee empl = this.employeeRepository.insert(employee);
+      return empl;
+    } else {
+      throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+    }
+
   }
 
   @PutMapping("/{id}")
-  public void replace(@RequestBody Employee employee, @PathVariable String id) {
-    Employee employeeFromDb = employeeService.getEmployee(id);
-    employee.getPermissions().getLifeguard().setImage(
-        employeeFromDb.getPermissions().getLifeguard().getImage()
-    );
-    employeeRepository.save(employee);
+  public void replace(@RequestBody Employee employee, @PathVariable String id, @RequestHeader("Authorization") String auth) {
+    if (authService.isAuthorization(auth)) {
+      Employee employeeFromDb = employeeService.getEmployee(id);
+      employee.getPermissions().getLifeguard().setImage(
+          employeeFromDb.getPermissions().getLifeguard().getImage()
+      );
+      employeeRepository.save(employee);
+    } else {
+      throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+    }
+
   }
 }
